f58cd4156a59294008cff45c5d88f396c883431e
BaUka0 <bauirzhan.bisanov@gmail.com>
2026-03-03 02:05:53 +0500

    fix: updated topic model

---
+++ src/AWM.Service.Infrastructure/Migrations/20260302050901_AddIsSubmittedForApprovalToTopics.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AWM.Service.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsSubmittedForApprovalToTopics : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSubmittedForApproval",
+                schema: "Thesis",
+                table: "Topics",
+                type: "bit",
+                nullable: false,
+                defaultValue: false)
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "TopicsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "Thesis")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "SysEndTime")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "SysStartTime");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSubmittedForApproval",
+                schema: "Thesis",
+                table: "Topics")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "TopicsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "Thesis")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "SysEndTime")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "SysStartTime");
+        }
+    }
+}
